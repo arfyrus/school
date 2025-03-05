71308f089f4b26a478a1ce5c9a8b32129758a00a
Arif Hafeez <arif.hafeez@icloud.com>
2025-03-05 22:29:57 +0800

    Added

---
+++ kerja-kursus/tempahan/login.php
@@ -0,0 +1,51 @@
+<?php
+    include("sambungan.php");
+    include("pelanggan_menu.php");
+
+    if (isset$_POST["submit"])) {
+        $userid = $_POST["userid"];
+        $password = $_POST["password"];
+
+        $jumpa = FALSE;
+
+        if ($jumpa == FALSE) {
+            $sql = "SELECT * FROM pelanggan";
+            $result = mysqli_query($sambungan, $sql);
+            while ($pelanggan["idpelanggan"] == $userid && $pelanggan["password"] == $password) {
+                $jumpa = TRUE;
+                $_SESSION["idpelanggan"] = $jurujual["idjurujual"];
+                $_SESSION["nama"] = $pelanggan["nama"];
+                $_SESSION["status"] = "jurujual";
+                break;
+            }
+        }
+
+        if ($jumpa == TRUE) {
+            if ($_SESSION["status"] == "pelanggan") {
+                header("Location: index.php");
+            } else if ($_SESSION["status"] == "jurujual") {
+                header("Location: makanan_senarai.php");
+            }
+        echo "<script>alert('ID pengguna atau kata laluan salah!');</script>";
+        }
+    }
+?>
+
+<link rel="stylesheet" href="abutton.css">
+<link rel="stylesheet" href="aborang.css">
+
+<h3 class="pendek">LOG IN</h3>
+<form class="pendek" action="login.php" method="post">
+    <table>
+        <tr>
+            <td><img src="imej/user.png"></td>
+            <td><input type="text" name="userid" placeholder="idpengguna"></td>
+        </tr>
+        <tr>
+            <td><img src="imej/lock.png"></td>
+            <td><input type="password" name="password" placeholder="katalaluan"></td>
+        </tr>
+    </table>
+    <button class="login" type="submit" name="submit">Login</button>
+    <button class="signup" type="button" onclick="window.location='signup.php'">Sign Up</button>
+</form>
